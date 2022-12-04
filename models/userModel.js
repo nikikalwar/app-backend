87b42d005a812a7e806c661e7967102ac7076f5c
Niki Sah <nikikumar27@gmail.com>
2022-12-04 21:30:02 +0530

    added functionality for user-signup

--- models/userModel.js
+++ models/userModel.js
@@ -9,11 +9,22 @@ const userSchema = new mongoose.Schema(
     }
     );
 
-    const Users = mongoose.model('Users', userSchema);
+    const userSignUpSchema = new mongoose.Schema(
+      { 
+      firstName:String,
+      lastName:String,
+      age:Number,
+      phone:Number,  
+      password:{type:String,required:false},
+      email:{type:String,required:false}
+      }
+      );
+  
+    const Users = mongoose.model('Users', userSignUpSchema);
     main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://root:K4bHbxJBVdLdvug@cluster0.kvrazdn.mongodb.net/test');
+  await mongoose.connect('mongodb+srv://root:K4bHbxJBVdLdvug@cluster0.kvrazdn.mongodb.net/test-users');
    
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
